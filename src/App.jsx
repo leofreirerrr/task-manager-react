1b52c3c7860cd8afac41e679eb36f1537e118966
leofreirerrr <leofreirerr@gmail.com>
2025-02-05 00:36:54 -0300

    botão de deleta pronto

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "Estudar React para melhorar minhas habilidades",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Cálculo",
+      description: "só um exemplo",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Treinar a mira",
+      description: "Treinar a mira pro marvel rivals",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTaks = tasks.map((task) => {
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTaks);
+  }
+  function onDeleteTaskCLick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+    console.log("oi");
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-red-500 text-3xl">Gerenciador de Tarefas</h1>
+        <AddTask />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskCLick={onDeleteTaskCLick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;

64c18593488a6993df19fc7239f5f87bb99392b4
leofreirerrr <leofreirerr@gmail.com>
2025-02-05 15:56:19 -0300

    deposi eu continuo essa porra

--- src/App.jsx
+++ src/App.jsx
@@ -42,8 +42,8 @@ function App() {
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
-        <h1 className="text-red-500 text-3xl">Gerenciador de Tarefas</h1>
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
         <AddTask />
         <Tasks
           tasks={tasks}
